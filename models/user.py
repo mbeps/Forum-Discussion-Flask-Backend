8f9ab7e0c203abfef92564cd5605e504767394bd
Maruf Bepary <bepary71@gmail.com>
2022-10-31 14:29:48 +0000

    Created user model

---
+++ models/user.py
@@ -0,0 +1,12 @@
+from app import db
+from models import AppModel
+
+
+class User(db.Model, AppModel):
+    __tablename__ = 'user'
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20))
+    email = db.Column(db.String(256))
+    password = db.Column(db.String(20))
+    is_authenticated = db.Column(db.Boolean, default=False)
+    code = db.Column(db.String(20))
