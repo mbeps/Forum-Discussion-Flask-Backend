a29e8a77023de285bc89fbc93f8e3d63ad87e247
Maruf Bepary <bepary71@gmail.com>
2022-11-26 19:49:55 +0000

    Increased password field length - Encryption requires more characters

--- models/user.py
+++ models/user.py
@@ -7,6 +7,6 @@ class User(db.Model, AppModel):
     user_id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20))
     email = db.Column(db.String(256))
-    password = db.Column(db.String(20))
+    password = db.Column(db.String(255))
     is_authenticated = db.Column(db.Boolean, default=False)
     code = db.Column(db.String(20))
