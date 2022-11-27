b6a9cb665c880abb732a85ea59d0c9ac3c8c85f2
Maruf Bepary <bepary71@gmail.com>
2022-11-27 16:37:54 +0000

    Created `community` table

---
+++ models/community.py
@@ -0,0 +1,10 @@
+from app import db
+from models import AppModel
+
+
+class Community(db.Model, AppModel):
+    __tablename__ = 'communities'
+    community_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    community_name = db.Column(db.String(20))
+    description = db.Column(db.String(256))
