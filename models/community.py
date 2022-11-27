f1cc46b4e35445145c664af2bbf7c4bc27fc0c76
Maruf Bepary <bepary71@gmail.com>
2022-11-27 16:44:10 +0000

    Return all communities

--- models/community.py
+++ models/community.py
@@ -8,3 +8,8 @@ class Community(db.Model, AppModel):
     user_id = db.Column(db.Integer)
     community_name = db.Column(db.String(20))
     description = db.Column(db.String(256))
+
+    @staticmethod
+    def get_all_communities():
+        return Community.query.filter()
+
