779d38b7cc72e9356b05cfb6d140ad934eb8314d
Maruf Bepary <bepary71@gmail.com>
2022-11-28 18:35:07 +0000

    Added documentation - Commented functionality - Type hints - Docstring

--- api/community.py
+++ api/community.py
@@ -1,4 +1,4 @@
-from flask import make_response, jsonify, request, redirect
+from flask import make_response, jsonify, request, redirect, Request, Response
 from sqlalchemy import text, engine
 
 from app import app
@@ -6,17 +6,20 @@ from models import Community, CommunitySubscribe
 
 
 @app.route('/add_community', methods=['POST'])
-def add_community():
-    community = request.get_json()
-    user_id = community.get('user_id')
-    community_name = community.get('community_name')
-    description = community.get('description')
+def add_community() -> Response:
+    """Creates a new community. 
+    The user who creates the community is automatically subscribed to it.
 
-    community = Community(user_id=user_id, community_name=community_name, description=description)
-    community.save()
-    cs = CommunitySubscribe(user_id=user_id, community_id=community.community_id)
-    cs.save()
+    Returns:
+        Response: whether the community was created or not
+    """    
+    community = request.get_json() # get json data from request
+    user_id: str = community.get('user_id') # get user_id from json data
+    community_name: str = community.get('community_name') # get community_name from json data
+    description: str = community.get('description') # get description from json data
 
-    return make_response(jsonify({'msg': 'Community has been added'}), 200)
-
-
+    community: Community = Community(user_id=user_id, community_name=community_name, description=description) # create community object
+    community.save() # save community object to database
+    cs: CommunitySubscribe = CommunitySubscribe(user_id=user_id, community_id=community.community_id) # community creator is automatically subscribed to community
+    cs.save() # save community subscribe object to database
+    return make_response(jsonify({'msg': 'Community has been added'}), 200) # return success message
