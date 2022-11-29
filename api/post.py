c9884a76e627ff155190ad92f239904f18b3ea80
Maruf Bepary <bepary71@gmail.com>
2022-11-29 12:15:58 +0000

    Gets all posts a user is subscribed to - Along with some useful metadata about post

--- api/post.py
+++ api/post.py
@@ -21,3 +21,60 @@ def create_post() -> Response:
     post.save() # save the post to the database
 
     return make_response(jsonify({'msg': 'post has been created'}), 200) # return a response to the user
+
+
+def all_subscribed_community_posts(user_id: int) -> list[dict[str]]:
+    """Find all posts in communities that a user is subscribed to.
+
+    Args:
+        user_id (int): user for which to find posts
+
+    Returns:
+        list[dict[str]]: list of posts
+    """    
+    mycursor.execute('''
+    select cp.post_id, cp.post_name, cp.description, u.username, c.community_name, cp.create_dttm, c.community_id from posts cp
+    join user u on u.user_id = cp.user_id
+    join communities c on c.community_id = cp.community_id
+    order by cp.create_dttm desc
+    -- where cp.user_id = %s
+    ''') # get all posts from all communities the user is subscribed to
+    # ''', (user_id,))
+    data: list = mycursor.fetchall() # fetch all the data
+    mydb.commit() # commit the changes to the database
+    return data # return the data
+
+
+@app.route('/all_posts', methods=['POST'])
+def get_all_posts() -> Response:
+    """Gets all posts from all communities the user is subscribed to along with other data. 
+    Each post will have:
+    - post_id
+    - post_name
+    - description
+    - username
+    - community_name
+    - total_likes
+    - posted_time
+
+    Returns:
+        Response: all posts from all communities the user is subscribed to
+    """    
+    user = request.get_json() # get the user data
+    user_id: int = user.get('user_id') # get the user id
+    posts: list = all_subscribed_community_posts(user_id) # get all posts from all communities the user is subscribed to
+    all_posts: list[dict] = [] # create an empty list to store all posts
+    for post in posts: # loop through all posts
+        likes = LikePost.query.filter(LikePost.post_id == post[0]).count() # get the number of likes for the post
+        is_subscribed = CommunitySubscribe.query.filter_by(community_id=post[6], user_id=user_id).first() # check if the user is subscribed to the community
+        if is_subscribed: # if the user is subscribed to the community 
+            all_posts.append({ 
+                'post_id': post[0],
+                'post_name': post[1],
+                'description': post[2],
+                'username': post[3],
+                'community_name': post[4],
+                "total_likes": likes,
+                "posted_time": post[5]
+            }) # append the post to the list of posts
+    return make_response(jsonify(all_posts), 200) # return the list of posts to the user
